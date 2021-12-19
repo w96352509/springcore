06440c8ed786921fcd0fb67fb0318b2241269dfd
vic <vichsu38@gmail.com>
2021-12-19 14:17:06 +0800

    s

---
+++ springcore/src/main/java/com/study/springcore/case05/Teacher.java
@@ -0,0 +1,10 @@
+package com.study.springcore.case05;
+
+import java.util.Set;
+
+public class Teacher {
+
+	private Integer id; //講師代號
+	private String name;//姓名
+	private Set<Clazz> clazzs ; //授課項目
+}
